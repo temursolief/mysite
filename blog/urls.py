1fa1a9a416f544137d4c2cf212f14b1af36d6784
Timur Soliev <temursoliev2110@gmail.com>
2022-09-25 13:16:46 -0500

    feat: added email sending view and forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import (post_list, post_detail, 
+					post_share, PostListView)
 
 app_name = 'blog'
 
 urlpatterns = [
-	path('', post_list, name='post_list'),
+	path('', PostListView.as_view(), name='post_list'),
 	path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
 				post_detail, name='post_detail'),
+	path("<int:post_id>/share/", post_share, name='post-share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import (Paginator, EmptyPage,
 									PageNotAnInteger)
+from django.core.mail import send_mail
+from .models import Post
+from .forms import EmailPostForm
+
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = "posts"
+	paginate_by = 3
+	template_name = "blog/post/list.html"
 
 
 def post_list(request):
@@ -32,3 +42,28 @@ def post_detail(request, year, month, day, post):
 			'blog/post/detail.html',
 			{'post': post})
 
+
+def post_share(request, post_id):
+	post = get_object_or_404(Post, 
+			id=post_id, 
+			status=Post.Status.PUBLISHED)
+	sent = False
+
+	if request.method == "POST":
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(
+				post.get_absolute_url())
+			subject = f"{cd['name']} recommends you read"\
+					f"{ post.title }"
+			message = f"Read {post.title} at {post.url}\n\n" \
+					f"{cd['name']}\'s comments: {cd['comments']}"
+			send_mail(subject, message, cd['email'], 
+					[cd['to']])
+			sent = True
+	else:
+		form = EmailPostForm()
+	return render(request,
+			'blog/post/share.html',
+			{"post": post, 'form': form, 'sent': sent})
